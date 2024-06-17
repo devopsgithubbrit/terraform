470f928f4f0d3f82b10f7afbf5d016e0a3c9cd33
Vinuthna <vinnuch2408@gmail.com>
2024-06-17 16:44:21 -0700

    locals

--- locals/variables.tf
+++ locals/variables.tf
@@ -1,17 +1,9 @@
-variable "instance_names" {
-    type=list
-    default=["mongodb","redis","mysql","rabbitmq","catalogue","user","cart","shipping","payment","dispatch","web"]
+variable "isProd" {
+    type=bool
+    default=True
 }
 
 variable "ami_id" {
     type=string
     default= "ami-0b4f379183e5706b9"
-}
-
-variable "zone_id" {
-    default= "Z09079071I9PB2SGNM2HI"
-}
-
-variable "domain_name" {
-    default = "chintu.cloud"
 }
